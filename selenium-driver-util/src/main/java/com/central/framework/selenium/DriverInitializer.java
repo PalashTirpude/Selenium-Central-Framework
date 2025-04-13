f68990000317e1b927803993943a82b6354f845b
Palash Tirpude <palashtirpude100@gmail.com>
2025-04-13 07:58:04 +0530

    refactor(driver-init): replace loadProperties method with LoadProperties utility from generic-utils

--- selenium-driver-util/src/main/java/com/central/framework/selenium/DriverInitializer.java
+++ selenium-driver-util/src/main/java/com/central/framework/selenium/DriverInitializer.java
@@ -1,15 +1,11 @@
 package com.central.framework.selenium;
 
+import com.central.framework.genericutils.LoadProperties;
 import com.central.framework.selenium.enums.Browsers;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.Objects;
 import java.util.Properties;
 
 @Slf4j
@@ -18,17 +14,10 @@ public class DriverInitializer {
     private static final ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
     @Getter
     private static Properties properties;
-    private static final String propertyFileName = "driver-config.properties";
 
-    public static void initializeWebDriver(String browserName) {
+    public static void initializeWebDriver(String browserName,String filePath) {
         DriverManager driverManager = new DriverManager();
-        try {
-            loadDriverProperty();
-        } catch (URISyntaxException | IOException e) {
-            log.error("Error loading driver property file", e);
-            throw new RuntimeException("Failed to load properties file", e);
-        }
-
+        properties=LoadProperties.fromFile(filePath);
         driverManager.setProperties(properties);
         WebDriver driver = driverManager.getWebDriver(Browsers.valueOf(browserName));
         driverThreadLocal.set(driver);
@@ -47,24 +36,6 @@ public class DriverInitializer {
         if (driver != null) {
             driver.quit();
             driverThreadLocal.remove();
-        }
-    }
-
-    private static void loadDriverProperty() throws URISyntaxException, IOException {
-        if (properties == null) {
-            String propertiesPath = System.getProperty("driver.config.path");
-            properties = new Properties();
-            File propertyFile;
-
-            if (propertiesPath != null && !propertiesPath.isEmpty()) {
-                propertyFile = new File(propertiesPath);
-            } else {
-                propertyFile = new File(Objects.requireNonNull(
-                        Thread.currentThread().getContextClassLoader().getResource(propertyFileName)
-                ).toURI());
-            }
-
-            properties.load(new FileInputStream(propertyFile));
         }
     }
 
